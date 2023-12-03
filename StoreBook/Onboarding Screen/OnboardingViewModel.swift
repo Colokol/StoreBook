da0da397cd4dac9ddc64441b62a9580c6bac256b
Dmitriy <d1386838@gmail.com>
2023-12-04 00:41:46 +0300

    created MVVM pattern for Onboarding Screen, added book assets to project

---
+++ StoreBook/Onboarding Screen/OnboardingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingViewModel.swift
+//  StoreBook
+//
+//  Created by Дмитрий on 04.12.2023.
+//
+
+import UIKit
+
+class OnboardingViewModel {
+    //  должна содержать бизнес-логику и подготавливать данные для отображения, а также реагировать на действия пользователя.
+}
