592c65ce33565bbe830c48ff1deb18bee120463e
DmKeller <dmit.keller@gmail.com>
2023-12-05 23:48:22 +0300

    fix UI

--- StoreBook/Screens/Categories/View/CategoriesViewController.swift
+++ StoreBook/Screens/Categories/View/CategoriesViewController.swift
@@ -1,45 +1,105 @@
-
 import UIKit
 
 final class CategoriesViewController: UIViewController {
+    // MARK: - Private properties
     private var collectionView: UICollectionView!
-     private var viewModel = CategoriesViewModel()
-     private let cellID = "CategoryCell"
+    private var viewModel = CategoriesViewModel()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Categories"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         setupCollectionView()
-         viewModel.fetchCategories()
-     }
-     
-     private func setupCollectionView() {
-         let layout = UICollectionViewFlowLayout()
-         layout.scrollDirection = .vertical
-         layout.minimumLineSpacing = 10
-         
-         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
-         collectionView.backgroundColor = .white
-         collectionView.dataSource = self
-         collectionView.delegate = self
-         collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: cellID)
-         
-         view.addSubview(collectionView)
-     }
- }
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        viewModel.fetchCategories()
+    }
 
- extension CategoriesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-         return viewModel.categories.count
-     }
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        setupCollectionView()
+    }
 
-     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? CategoryCell else {
-             return UICollectionViewCell() 
-         }
-         
-         let category = viewModel.categories[indexPath.item]
-         cell.configure(with: category)
-         
-         return cell
-     }
+    private func setupCollectionView() {
+        let layout = createLayout()
+        configureCollectionView(with: layout)
+        registerCollectionViewCells()
+        addCollectionViewConstraints()
+    }
+
+    private func configureCollectionView(with layout: UICollectionViewLayout) {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+    }
+
+    private func registerCollectionViewCells() {
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.cellID)
+    }
+
+    private func addCollectionViewConstraints() {
+    
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 168),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 232),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    // MARK: - UICollectionViewCompositionalLayout
+    private func createLayout() -> UICollectionViewLayout {
+        // Определение размеров ячейки
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(150), heightDimension: .absolute(100))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+       // item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        // Определение группы, в которой будут расположены ячейки
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(100))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(20)
+
+        // Определение раздела с этой группой
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20)
+        section.interGroupSpacing = 20
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension CategoriesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.categories.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.cellID, for: indexPath) as? CategoryCell else {
+            return UICollectionViewCell()
+        }
+
+        let category = viewModel.categories[indexPath.item]
+        cell.configure(with: category)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension CategoriesViewController: UICollectionViewDelegate {
+ 
 }
--- StoreBook/Screens/Categories/View/Cell/CategoryCell.swift
+++ StoreBook/Screens/Categories/View/Cell/CategoryCell.swift
@@ -1,19 +1,28 @@
 import UIKit
 
 final class CategoryCell: UICollectionViewCell {
+    static let cellID = String(describing: CategoryCell.self)
     
     private let categoryTitleLabel: UILabel = {
         let label = UILabel()
+        label.text = "Drama"
+        label.textColor = UIColor.white
         label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let categoryImage: UIImageView = {
         let img = UIImageView()
+        img.contentMode = .scaleToFill
+        img.clipsToBounds = true
+        img.layer.cornerRadius = 5
+        img.translatesAutoresizingMaskIntoConstraints = false
         return img
     }()
     
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -23,13 +32,36 @@ final class CategoryCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - UI Setup
     private func setupUI() {
+        addSubview(categoryImage)
         addSubview(categoryTitleLabel)
         
+        setupGradient()
+        
+        NSLayoutConstraint.activate([
+            categoryImage.topAnchor.constraint(equalTo: topAnchor),
+            categoryImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            categoryImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            categoryImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            categoryTitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            categoryTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])  
+    }
+    
+    private func setupGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        ]
+        gradientLayer.frame = bounds
+        categoryImage.layer.addSublayer(gradientLayer)
     }
     
     func configure(with category: CategoryModel) {
         categoryTitleLabel.text = category.title
-        
+        categoryImage.image = UIImage(named: category.image)
     }
 }
