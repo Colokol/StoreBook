7d9134ed4838812074fb90b5d8f62b736d0d2062
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-05 12:21:06 +0100

    create folder and files

---
+++ StoreBook/BookDetails/View/DetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailsViewController.swift
+//  StoreBook
+//
+//  Created by Kirill Taraturin on 05.12.2023.
+//
+
+import UIKit
+
+final class DetailsViewController: UIViewController {
+    
+    // MARK: - Private UI Properties
+    private lazy var nameLabel = DetailsViewBuilder.makeNameLabel()
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setViews()
+        setupConstraints()
+    }
+    
+    // MARK: - Private Methods
+    private func setViews() {
+        
+    }
+    
+    
+    private func setupConstraints() {
+        
+    }
+    
+    
+    
+}
