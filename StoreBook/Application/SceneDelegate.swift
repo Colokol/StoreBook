6f834edfda324adad392ae8d7aa3ecfec0e77d05
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-05 14:55:18 +0100

    setup UI with MVVM

--- StoreBook/Application/SceneDelegate.swift
+++ StoreBook/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = DetailsViewController()
+        window?.rootViewController = UINavigationController(rootViewController: DetailsViewController()) 
         window?.makeKeyAndVisible()
         
     }
--- StoreBook/BookDetails/ViewBuilder/DetailsViewBuilder.swift
+++ StoreBook/BookDetails/ViewBuilder/DetailsViewBuilder.swift
@@ -8,10 +8,52 @@
 import UIKit
 
 final class DetailsViewBuilder {
-    static func makeNameLabel() -> UILabel {
+    static let shared = DetailsViewBuilder()
+    private init() {}
+    
+    func makeNameLabel() -> UILabel {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont.boldSystemFont(ofSize: 28)
         label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }
+    
+    func makeImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .gray
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }
+    
+    func makeInfoLabel(with font: UIFont = UIFont.systemFont(ofSize: 14), numberOfLines: Int = 1) -> UILabel {
+        let label = UILabel()
+        label.font = font
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = numberOfLines
+        return label
+    }
+    
+    func makeStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }
+    
+    func makeButton(with title: String, with color: UIColor) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = color
+        let titleColor = color == UIColor.gray ? UIColor.black : .white
+        button.setTitleColor(titleColor, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return button
     }
 }
