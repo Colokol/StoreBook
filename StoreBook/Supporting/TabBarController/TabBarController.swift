49c958feb78d160c106757bf4bff81744b316f68
Colokol <Colokol1@gmail.com>
2023-12-06 22:01:07 +0300

    v.01

--- StoreBook/Supporting/TabBarController/TabBarController.swift
+++ StoreBook/Supporting/TabBarController/TabBarController.swift
@@ -1,70 +1,52 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    
-    // MARK: - LifeCycle
+
+        // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setupViewControllers()
-        
         configureTabBarAppearance()
     }
-    
+
     private func configureTabBarAppearance() {
         let appearance = UITabBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
-        
+
         appearance.stackedLayoutAppearance.normal.iconColor = .black
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.black]
         tabBar.scrollEdgeAppearance = appearance
         tabBar.tintColor = .black
-        
+
     }
-    
+
     private func setupViewControllers() {
-        let tabBarItemData = [
-            (
-                backgroundColor: UIColor.red,
-                title: "Home",
-                imageName: "home",
-                selectedImageName: "selectedHome"
-            ),
-            (
-                backgroundColor: UIColor.green,
-                title: "Categories",
-                imageName: "categories",
-                selectedImageName: "selectedCategories"
-            ),
-            (
-                backgroundColor: UIColor.blue,
-                title: "Likes",
-                imageName: "likes",
-                selectedImageName: "selectedLikes"
-            ),
-            (
-                backgroundColor: UIColor.yellow,
-                title: "Account",
-                imageName: "account",
-                selectedImageName: "selectedAccount"
-            )
-        ]
-        
-        let viewControllers = tabBarItemData.enumerated().map { index, data in
-            let viewController = UIViewController()
-            viewController.view.backgroundColor = data.backgroundColor
-            viewController.tabBarItem = UITabBarItem(
-                title: data.title,
-                image: UIImage(named: data.imageName),
-                tag: index
-            )
-            viewController.tabBarItem.selectedImage = UIImage(named: data.selectedImageName)
-            
-            return viewController
-        }
-        
-        self.viewControllers = viewControllers
+
+        let vc1 = UINavigationController(rootViewController: UIViewController())
+        let vc2 = UINavigationController(rootViewController: UIViewController())
+        let vc3 = UINavigationController(rootViewController: UIViewController())
+        let vc4 = UINavigationController(rootViewController: UIViewController())
+
+        vc1.title = "Home"
+        vc2.title = "Categories"
+        vc3.title = "Likes"
+        vc4.title = "Account"
+
+        vc1.tabBarItem.image = UIImage(named: "home")
+        vc2.tabBarItem.image = UIImage(named: "categories")
+        vc3.tabBarItem.image = UIImage(named: "likes")
+        vc4.tabBarItem.image = UIImage(named: "account")
+
+        vc1.tabBarItem.selectedImage = UIImage(named: "selectedHome")
+        vc2.tabBarItem.selectedImage = UIImage(named: "selectedCategories")
+        vc3.tabBarItem.selectedImage = UIImage(named: "selectedLikes")
+        vc4.tabBarItem.selectedImage = UIImage(named: "selectedAccount")
+
+        tabBar.tintColor = .black
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
     }
-    
+
 }
 
