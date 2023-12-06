2acd5e90ee35ac5b2131aa678b4c62182799c29c
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-06 18:57:35 +0100

    commented code for window

--- StoreBook/Application/SceneDelegate.swift
+++ StoreBook/Application/SceneDelegate.swift
@@ -14,11 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: DetailsViewController()) 
-        window?.makeKeyAndVisible()
-        
+        guard let _ = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: windowScene)
+//        window?.rootViewController = UINavigationController(rootViewController: DetailsViewController())
+//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
