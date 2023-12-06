04e7e39521e2e7b653ea67e39efbe42a183b0425
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-06 18:58:25 +0100

    delete windowScene

--- StoreBook/Application/SceneDelegate.swift
+++ StoreBook/Application/SceneDelegate.swift
@@ -15,9 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let _ = (scene as? UIWindowScene) else { return }
-//        window = UIWindow(windowScene: windowScene)
-//        window?.rootViewController = UINavigationController(rootViewController: DetailsViewController())
-//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
