9fba33ffe510e5a0f5c70c74368172d4eb13c374
Dmitriy <d1386838@gmail.com>
2023-12-06 17:04:58 +0300

    removed unused assets, added: UI elements and extensions

--- StoreBook/Onboarding Screen/OnboardingViewModel.swift
+++ StoreBook/Onboarding Screen/OnboardingViewModel.swift
@@ -4,25 +4,27 @@ import UIKit
 class OnboardingViewModel {
     
     let contentView: OnboardingModel
+    init(contentView: OnboardingModel) {
+        self.contentView = contentView
+    }
     public var currentTextIndex = 0
     
     var currentText: String {
         return contentView.description[currentTextIndex]
     }
     
-    init(contentView: OnboardingModel) {
-        self.contentView = contentView
-    }
-    
     func nextText() {
-        if currentTextIndex < contentView.description.count - 1 {
+        if currentTextIndex < contentView.description.count - 1  {
             currentTextIndex += 1
+            print (currentTextIndex)
+            
         } else {
             currentTextIndex = 0
         }
     }
     
     func shouldNavigateToNextScreen() -> Bool {
-        return currentTextIndex == contentView.description.count - 1
+        return currentTextIndex == 3
+        
     }
 }
