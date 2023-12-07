eef41f35c5919a4a7e02c2bb9b8db7a9115333d2
DmKeller <dmit.keller@gmail.com>
2023-12-08 01:13:53 +0300

    fix Category

--- StoreBook/Screens/Categories/View/SearchCategoriesViewController.swift
+++ StoreBook/Screens/Categories/View/SearchCategoriesViewController.swift
@@ -25,17 +25,7 @@ extension SearchCategoriesViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return selectedBook.count
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        
-        guard let workCell = cell as? WorkTableViewCell else { return UITableViewCell() }
-        let picture = selectedImage?[indexPath.row]
-        workCell.titleLabel.text = picture?.title
-        workCell.picturesImageView.image = UIImage(named: picture?.image ?? "")
-        return workCell
-    }
+
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
