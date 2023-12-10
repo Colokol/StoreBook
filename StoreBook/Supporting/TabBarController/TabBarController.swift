fa22f9a04fb357f0c17581d2ed82a2b1366cfd21
Nodnet <nodnet@mail.ru>
2023-12-10 16:21:50 +0300

    HomeScreen

--- StoreBook/Supporting/TabBarController/TabBarController.swift
+++ StoreBook/Supporting/TabBarController/TabBarController.swift
@@ -24,7 +24,7 @@ final class TabBarController: UITabBarController {
 
     private func setupViewControllers() {
 
-        let vc1 = UINavigationController(rootViewController: UIViewController())
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: CategoriesViewController())
         let vc3 = UINavigationController(rootViewController: UIViewController())
         let vc4 = UINavigationController(rootViewController: UIViewController())
