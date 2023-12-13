e14affd0159739d4d00159d90a822f0eac07daa6
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-13 14:02:46 +0100

    create storageManager instance in sceneDelegate

--- StoreBook/Application/SceneDelegate.swift
+++ StoreBook/Application/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    private var storageManager = StorageManager.shared
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
